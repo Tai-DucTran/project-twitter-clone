328e55807b23f1963d25688c3a98ce56220ccbd4
Tai-DucTran <taitran.deve@gmail.com>
2023-09-30 15:26:28 +0700

    package: update packages/libraries

--- lib/services/firestore_tweet_service/models/profile_post_model.dart
+++ lib/services/firestore_tweet_service/models/profile_post_model.dart
@@ -43,8 +43,8 @@ class _ProfilePostModelState extends State<ProfilePostModel> {
             ListTile(
               dense: true,
               // leading - User's Avata
-              leading: Column(
-                children: const [
+              leading: const Column(
+                children:  [
                   Icon(
                     Icons.supervised_user_circle_outlined,
                     size: 45,
