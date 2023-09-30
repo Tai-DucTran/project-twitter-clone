209b7d68be9aa37f7bac63a24f34beeda94b18d6
Tai-DucTran <taitran.deve@gmail.com>
2023-09-30 18:29:39 +0700

    main: update packages/libraries

--- lib/views/profile/edit_profile_view.dart
+++ lib/views/profile/edit_profile_view.dart
@@ -1,6 +1,5 @@
 import 'package:finalproject/services/firestore_user_service/user_firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
--- lib/views/user_account/register_view.dart
+++ lib/views/user_account/register_view.dart
@@ -1,10 +1,9 @@
-//  RegisterView:
+
 import 'package:finalproject/constants/routes.dart';
 import 'package:finalproject/services/firebase_auth_service/auth_exceptions.dart';
 import 'package:finalproject/services/firebase_auth_service/auth_service.dart';
 import 'package:finalproject/utilities/show_error_dialog.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
