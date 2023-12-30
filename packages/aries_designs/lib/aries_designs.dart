251e18aba7595fc96f18445df650c92a7ca8ace5
Tai-DucTran <taitran.deve@gmail.com>
2023-12-30 16:26:21 +0700

    aries_design: created storybook.dart

--- packages/aries_designs/lib/aries_designs.dart
+++ packages/aries_designs/lib/aries_designs.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:storybook_flutter/storybook_flutter.dart';
 
-class AriesDesignStoryBook extends StatelessWidget {
-  const AriesDesignStoryBook({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Storybook(
-      stories: const [],
-    );
-  }
-}
