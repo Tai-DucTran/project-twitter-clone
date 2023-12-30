5b39b3031abccb29848f09e234036de3986c626b
Tai-DucTran <taitran.deve@gmail.com>
2023-12-30 16:12:56 +0700

    StoryBook: created

--- packages/aries_designs/lib/aries_designs.dart
+++ packages/aries_designs/lib/aries_designs.dart
@@ -1,7 +1,13 @@
-library aries_designs;
+import 'package:flutter/material.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class AriesDesignStoryBook extends StatelessWidget {
+  const AriesDesignStoryBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Storybook(
+      stories: const [],
+    );
+  }
 }
