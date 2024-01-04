89a383a55c64ff20859ff8360e959267114ad188
Tai-DucTran <taitran.deve@gmail.com>
2024-01-04 23:24:55 +0700

    ProfileSection: added back profileRoute function

--- lib/src/modules/profile_drawer/molecules/profile_section.dart
+++ lib/src/modules/profile_drawer/molecules/profile_section.dart
@@ -1,7 +1,6 @@
 import 'package:aries_designs/aries_designs.dart';
 import 'package:finalproject/src/constants/routes.dart';
 import 'package:finalproject/src/modules/profile_drawer/atoms/following_and_followers.dart';
-import 'package:finalproject/src/modules/profile_drawer/organism/drawer_profile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +19,10 @@ class _ProfileSectionState extends State<ProfileSection> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         InkWell(
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil(profileRoute, (route) => false);
+          },
           child: const CircleAvatar(
             backgroundImage: NetworkImage("https://robohash.org/\$15?set=set2"),
             radius: 25,
