867ffd8ebe661a65b297c54b2a87c025f8886f6b
Tai-DucTran <taitran.deve@gmail.com>
2024-01-05 00:42:44 +0700

    Profile/widgets: created avatar_and_edit_profile_section

---
+++ lib/src/modules/profile/widgets/molecules/avatar_and_edit_profile_section.dart
@@ -0,0 +1,43 @@
+import 'package:finalproject/src/modules/profile/widgets/atoms/atoms.dart';
+import 'package:flutter/material.dart';
+
+class AvatarAndEditProfileSection extends StatelessWidget {
+  final String imageUrl;
+  final bool? isDartModeOn;
+
+  const AvatarAndEditProfileSection({
+    super.key,
+    required this.imageUrl,
+    this.isDartModeOn = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const double positionHeight = -25.0;
+    return Container(
+      transform: Matrix4.translationValues(
+        0.0,
+        positionHeight,
+        0.0,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatarInProfile(
+            positionHeight: positionHeight,
+            imageUrl: imageUrl,
+            radius: 40.0,
+            hasBackgroundBehind: true,
+          ),
+          EditProfileButton(
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/modules/profile/widgets/molecules/molecules.dart
@@ -0,0 +1,2 @@
+export 'avatar_and_edit_profile_section.dart';
+export 'cover_profile_section.dart';
