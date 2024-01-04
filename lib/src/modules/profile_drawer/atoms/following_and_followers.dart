409c1a079a7d4519636d76c03f5ce51f21311a02
Tai-DucTran <taitran.deve@gmail.com>
2024-01-04 22:58:40 +0700

    widgets: created following_and_followers

---
+++ lib/src/modules/profile_drawer/atoms/following_and_followers.dart
@@ -0,0 +1,53 @@
+import 'package:aries_designs/aries_designs.dart';
+import 'package:flutter/material.dart';
+
+class FollowingFollowersSection extends StatelessWidget {
+  final int followingNumber;
+  final int followerNumber;
+  final bool isDartModeOn;
+
+  const FollowingFollowersSection({
+    super.key,
+    required this.followingNumber,
+    required this.followerNumber,
+    required this.isDartModeOn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Color numberTextColor = isDartModeOn ? Colors.white : Colors.black;
+    final TextStyle numberTextStyle = Theme.of(context)
+        .textTheme
+        .bodyMedium!
+        .copyWith(color: numberTextColor, fontWeight: FontWeight.bold);
+
+    final TextStyle followingFollowerTextStyle = Theme.of(context)
+        .textTheme
+        .bodyMedium!
+        .copyWith(color: Colors.black, fontWeight: FontWeight.w500);
+
+    return Row(
+      children: [
+        Text(
+          '$followingNumber',
+          style: numberTextStyle,
+        ),
+        Spacing.sp4,
+        Text(
+          'Following',
+          style: followingFollowerTextStyle,
+        ),
+        Spacing.sp8,
+        Text(
+          '$followerNumber',
+          style: numberTextStyle,
+        ),
+        Spacing.sp4,
+        Text(
+          'Followers',
+          style: followingFollowerTextStyle,
+        ),
+      ],
+    );
+  }
+}
