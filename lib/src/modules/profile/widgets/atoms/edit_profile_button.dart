1ce23c3839be2200963341fc83d813cfc5842709
Tai-DucTran <taitran.deve@gmail.com>
2024-01-05 00:42:27 +0700

    Profile/widgets: create edit_profile_button

---
+++ lib/src/modules/profile/widgets/atoms/edit_profile_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EditProfileButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  const EditProfileButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 150,
+      padding: const EdgeInsets.only(top: 20, left: 35),
+      child: OutlinedButton(
+        onPressed: () {
+          onPressed;
+        },
+        child: Text(
+          'Edit profile',
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+      ),
+    );
+  }
+}
