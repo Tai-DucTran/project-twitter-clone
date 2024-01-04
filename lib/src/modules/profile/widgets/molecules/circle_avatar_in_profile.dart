83f1337dba4799c32a22bd86113fcb9e48f5b25a
Tai-DucTran <taitran.deve@gmail.com>
2024-01-04 22:59:34 +0700

    Profile: modified circle_avatar

--- lib/src/modules/profile/widgets/molecules/circle_avatar_in_profile.dart
+++ lib/src/modules/profile/widgets/molecules/circle_avatar_in_profile.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class CircleAvatarInProfile extends StatelessWidget {
   final String imageUrl;
-  final bool isDartModeOn;
+  final bool? isDartModeOn;
+  final bool hasBackgroundBehind;
   final double radius;
   const CircleAvatarInProfile({
     super.key,
     required this.imageUrl,
-    required this.isDartModeOn,
+    this.isDartModeOn = false,
     required this.radius,
+    required this.hasBackgroundBehind,
   });
 
   static const double ratio = 9 / 4;
@@ -18,19 +20,21 @@ class CircleAvatarInProfile extends StatelessWidget {
     return Stack(
       alignment: Alignment.topCenter,
       children: [
-        Container(
-          width: radius * ratio,
-          height: radius * ratio,
-          transform: Matrix4.translationValues(
-            -140.0,
-            -35.0,
-            0.0,
-          ),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: isDartModeOn ? Colors.black : Colors.white,
-          ),
-        ),
+        hasBackgroundBehind
+            ? Container(
+                width: radius * ratio,
+                height: radius * ratio,
+                transform: Matrix4.translationValues(
+                  -140.0,
+                  -35.0,
+                  0.0,
+                ),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isDartModeOn! ? Colors.black : Colors.white,
+                ),
+              )
+            : const Offstage(),
         Container(
           transform: Matrix4.translationValues(
             -140.0,
